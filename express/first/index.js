fadd8599db79d9d2e17f2f53c83b625ebf0b8ab3
Srikrishna S Kashyap <skashyapsri04@gmail.com>
2018-07-21 02:27:44 +0530

    Express

--- express/first/index.js
+++ express/first/index.js
@@ -1,8 +1,6 @@
-var express = require('express');
-var app = express();
+const express = require('express')
+const app = express()
 
-app.get('/', function(req, res){
-   res.send("Hello world!");
-});
+app.get('/', (req, res) => res.send('Hello World!'))
 
-app.listen(3000);
+app.listen(3000, () => console.log('Example app listening on port 3000!'))
